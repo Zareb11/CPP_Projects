fbf623c2780a92bdbcb33f576847ebc12e77a940
Jacob <jacobrwitbooi@gmail.com>
2022-09-21 11:30:28 +0200

    Added another student to double linked list

--- StudentManagementSystem/StudentManagementSystem.cpp
+++ StudentManagementSystem/StudentManagementSystem.cpp
@@ -40,6 +40,7 @@ class Student {
                 }
                 cout << " - " << mark_ave/subject.second.size() << endl;
             }
+            cout << "-------------------" << endl;
         }
 };
 
@@ -74,20 +75,42 @@ int main()
     subject_marks.insert(pair<string, list<float>>("Computer Science", cs_marks));
     subject_marks.insert(pair<string, list<float>>("Mathematics", math_marks));
     subject_marks.insert(pair<string, list<float>>("Physics", phy_marks));
-
     Student student1 = Student("Jacob Witbooi", 3579072, "Computer Science", subjects, subject_marks);
     //student1.print_subjects();
     //student1.calculate_ave_subjects();
     //---------------------------------------------------------------------------------------------
+    list<string> subjects2 = {"Accounting", "Economics", "Commericial Law"};
+    list<float> acc_marks { 77, 79, 67, 71, 74 };
+    list<float> eco_marks { 67, 72, 81, 88, 80 };
+    list<float> cl_marks { 64, 71, 63, 73, 61 };
+    map<string, list<float>> subject_marks2;
+    subject_marks2.insert(pair<string, list<float>>("Accounting", cs_marks));
+    subject_marks2.insert(pair<string, list<float>>("Economics", math_marks));
+    subject_marks2.insert(pair<string, list<float>>("Commericial Law", phy_marks));
+    Student student2 = Student("Gdowinn Isaacs", 3579072, "Accounting", subjects2, subject_marks2);
+    //---------------------------------------------------------------------------------------------
     Node* head;
     Node* tail;
+    //---------------------------------------------------------------------------------------------
     Node* node = new Node();
     node->student = &student1;
     node->next = nullptr;
     node->prev = nullptr;
     head = node;
     tail = node;
-    preorderPrint(head);
+    //preorderPrint(head);
+    //---------------------------------------------------------------------------------------------
+    node = new Node();
+    node->student = &student2;
+    node->next = nullptr;
+    node->prev = tail;
+    tail->next = node;
+    tail = node;
+    //---------------------------------------------------------------------------------------------
+    student1.calculate_ave_subjects();
+    student2.calculate_ave_subjects();
+
+
 
 
 
